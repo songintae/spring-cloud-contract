cca968d465d6ec04608d26eb389c24b5e09a71a4
songintae <songintae92@hanmail.net>
2021-10-04 17:04:30 +0900

    Spring cloud Contract Verifier Plugin이 Producer만 적용되도록 변경

--- producer/src/test/java/com/example/producer/FraudControllerBaseTest.java
+++ producer/src/test/java/com/example/producer/FraudControllerBaseTest.java
@@ -2,9 +2,6 @@ package com.example.producer;
 
 import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.context.annotation.Bean;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class FraudControllerBaseTest {
 
